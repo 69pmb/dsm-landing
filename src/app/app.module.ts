9a5c5088a6686473878ca45c22cd7657e4e6d199
pmb <pm.broca@gmail.com>
2020-12-08 16:05:20 +0100

    feat: universal angular ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     AppRoutingModule,
   ],
